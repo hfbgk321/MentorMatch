06109d20cf7add50ac10dbde7f16b738ba2b454d
z-mingk <70775347+z-mingk@users.noreply.github.com>
2020-09-04 17:17:45 -0400

    hello

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-// hello jacky pt 2
+// hello jacky - zhimiing
 function App() {
   return (
     <div className="App">
