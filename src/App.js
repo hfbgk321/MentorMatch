a8a3ecf3c7346f7c134edd65269833e7c4f62672
Jie Zhang <jie.zhang.2@stonybrook.edu>
2020-09-04 16:26:46 -0400

    Jie Push

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Hello
+          Hello Jie
         </p>
         <a
           className="App-link"
